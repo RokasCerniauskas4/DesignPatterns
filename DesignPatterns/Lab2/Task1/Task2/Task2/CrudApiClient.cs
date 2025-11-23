1ed52d7b2a1e1af549a50baa1c6d7c81d2d40c82
Rokas Černiauskas <Rokascerniauskas4@gmail.com>
2025-11-23 13:10:11 +0200

    commitnk task2 part

---
+++ DesignPatterns/Lab2/Task1/Task2/Task2/CrudApiClient.cs
@@ -0,0 +1,61 @@
+﻿namespace Task2;
+
+public class CrudApiClient
+{
+    private readonly SomeEntityController _controller;
+
+    public CrudApiClient(SomeEntityController controller)
+    {
+        _controller = controller;
+    }
+    
+    
+    public SomeEntity Create(string name, string description)
+    {
+        var entity = new SomeEntity
+        {
+            Name = name,
+            Description = description
+        };
+
+        return _controller.Create(entity);
+    }
+    
+    public SomeEntity? GetOne(int id)
+    {
+        return _controller.GetOne(id);
+    }
+
+    public List<SomeEntity> GetMany()
+    {
+        return _controller.GetMany();
+    }
+
+    public void Update(int id, string name, string description, EntityStatus status)
+    {
+        var existing = _controller.GetOne(id);
+        if (existing == null)
+        {
+            return;
+        }
+
+        existing.Name = name;
+        existing.Description = description;
+        existing.Status = status;
+
+        _controller.Update(existing);
+    }
+    
+    public void Delete(int id)
+    {
+        _controller.Delete(id);
+    }
+    
+    public void DeleteMany(IEnumerable<int> ids)
+    {
+        _controller.DeleteMany(ids);
+    }
+
+
+    
+}
