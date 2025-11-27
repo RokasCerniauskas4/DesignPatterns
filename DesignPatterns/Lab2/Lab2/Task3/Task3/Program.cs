7ae0a1f421cf94ea024a73b4784c3bc19da3887d
Rokas Černiauskas <Rokascerniauskas4@gmail.com>
2025-11-27 16:38:13 +0200

    commitnk task2 part

--- DesignPatterns/Lab2/Lab2/Task3/Task3/Program.cs
+++ DesignPatterns/Lab2/Lab2/Task3/Task3/Program.cs
@@ -1,6 +1,29 @@
-﻿namespace task3;
-
-public class Program
+﻿namespace task3
 {
-    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user1 = new User("John", true, new List<int> { 1, 2 });
+            var user2 = new User("Peter", false, new List<int>());
+            var user3 = new User("Anna", true, new List<int> { 2 });
+
+            IBook bookForJohn = new BookProxy(1, user1);
+            IBook bookForPeter = new BookProxy(1, user2);
+            IBook bookForAnna = new BookProxy(1, user3);
+
+            Console.WriteLine("=== John tries to read book 1 ===");
+            Console.WriteLine(bookForJohn.Read());
+            Console.WriteLine();
+
+            Console.WriteLine("=== Peter tries to read book 1 (not registered) ===");
+            Console.WriteLine(bookForPeter.Read());
+            Console.WriteLine();
+
+            Console.WriteLine("=== Anna tries to read book 1 (no access) ===");
+            Console.WriteLine(bookForAnna.Read());
+            Console.WriteLine();
+
+        }
+    }
 }
--- DesignPatterns/Lab2/Lab2/Task3/Task3/User.cs
+++ DesignPatterns/Lab2/Lab2/Task3/Task3/User.cs
@@ -1,6 +1,18 @@
-﻿namespace task3;
-
-public class User
+﻿public class User
 {
-    
+    public string Name { get; }
+    public bool IsRegistered { get; }
+    public List<int> AllowedBookIds { get; }
+
+    public User(string name, bool isRegistered, List<int> allowedBookIds)
+    {
+        Name = name;
+        IsRegistered = isRegistered;
+        AllowedBookIds = allowedBookIds;
+    }
+
+    public bool CanRead(int bookId)
+    {
+        return AllowedBookIds.Contains(bookId);
+    }
 }
