f770e72d900447cf88021b30fef3daa4c51f27f5
Rokas Černiauskas <Rokascerniauskas4@gmail.com>
2025-12-03 16:10:55 +0200

    normalizing

--- DesignPatterns/Lab2/Lab2/task1/task1/Facade.cs
+++ DesignPatterns/Lab2/Lab2/task1/task1/Facade.cs
@@ -1,115 +1,68 @@
 ﻿namespace task1;
 
-
 public class TimerService
 {
     public void StartPomodoro(int minutes)
-    {
-        Console.WriteLine($"TimerService: starting Pomodoro for {minutes} minutes.");
-    }
+        => Console.WriteLine($"Timer: Pomodoro {minutes} min.");
 
     public void StartBreak(int minutes)
-    {
-        Console.WriteLine($"TimerService: starting break for {minutes} minutes.");
-    }
+        => Console.WriteLine($"Timer: break {minutes} min.");
 
     public void Stop()
-    {
-        Console.WriteLine("TimerService: stopping timer.");
-    }
+        => Console.WriteLine("Timer: stop.");
 }
 
 public class NotesService
 {
     public void OpenNotes(string subject)
-    {
-        Console.WriteLine($"NotesService: opening notes for '{subject}'.");
-    }
+        => Console.WriteLine($"Notes: open '{subject}'.");
 
     public void SaveNotes()
-    {
-        Console.WriteLine("NotesService: saving current notes.");
-    }
+        => Console.WriteLine("Notes: save.");
 
     public void CloseNotes()
-    {
-        Console.WriteLine("NotesService: closing notes.");
-    }
+        => Console.WriteLine("Notes: close.");
 }
 
 public class FocusService
 {
     public void EnableFocusMode()
-    {
-        Console.WriteLine("FocusService: enabling focus mode " +
-                          "(mute notifications, close socials).");
-    }
+        => Console.WriteLine("Focus: ON.");
 
     public void DisableFocusMode()
-    {
-        Console.WriteLine("FocusService: disabling focus" +
-                          " mode (notifications back on).");
-    }
+        => Console.WriteLine("Focus: OFF.");
 }
 
 public class MusicService
 {
     public void PlayFocusPlaylist()
-    {
-        Console.WriteLine("MusicService: playing focus playlist.");
-    }
+        => Console.WriteLine("Music: play focus playlist.");
 
     public void StopMusic()
-    {
-        Console.WriteLine("MusicService: stopping music.");
-    }
+        => Console.WriteLine("Music: stop.");
 }
-
 
 public class StudySessionFacade
 {
-    private readonly TimerService _timer;
-    private readonly NotesService _notes;
-    private readonly FocusService _focus;
-    private readonly MusicService _music;
-
-    public StudySessionFacade(
-        TimerService timer,
-        NotesService notes,
-        FocusService focus,
-        MusicService music)
-    {
-        _timer = timer;
-        _notes = notes;
-        _focus = focus;
-        _music = music;
-    }
+    private readonly TimerService _timer = new();
+    private readonly NotesService _notes = new();
+    private readonly FocusService _focus = new();
+    private readonly MusicService _music = new();
     
-    public void StartStudySession(string subject, int minutes)
+    public void Run(string subject, int focusMinutes = 25, int breakMinutes = 5)
     {
-        Console.WriteLine("=== StartStudySession() via StudySessionFacade ===");
-
+        Console.WriteLine("=== STUDY SESSION ===");
+        
         _focus.EnableFocusMode();
         _notes.OpenNotes(subject);
         _music.PlayFocusPlaylist();
-        _timer.StartPomodoro(minutes);
-    }
-    
-    public void TakeBreak(int minutes)
-    {
-        Console.WriteLine("=== TakeBreak() via StudySessionFacade ===");
-
-        _timer.StartBreak(minutes);
+        _timer.StartPomodoro(focusMinutes);
+        
+        _timer.StartBreak(breakMinutes);
         _music.StopMusic();
-        Console.WriteLine("You can stretch, drink water, check phone (just a bit 😏).");
-    }
-    
-    public void EndStudySession()
-    {
-        Console.WriteLine("=== EndStudySession() via StudySessionFacade ===");
-
+        Console.WriteLine("Break: you can relax now and drink couple of litres beer");
+        
         _timer.Stop();
-        _music.StopMusic();
         _notes.SaveNotes();
         _notes.CloseNotes();
         _focus.DisableFocusMode();
@@ -120,20 +73,8 @@ public static class FacadeDemo
 {
     public static void Run()
     {
-        var timer = new TimerService();
-        var notes = new NotesService();
-        var focus = new FocusService();
-        var music = new MusicService();
-
-        var study = new StudySessionFacade(timer, notes, focus, music);
-
-        study.StartStudySession("Design Patterns", 25);
-        Console.WriteLine();
-
-        study.TakeBreak(5);
-        Console.WriteLine();
-
-        study.EndStudySession();
-        Console.WriteLine();
+        var study = new StudySessionFacade();
+        
+        study.Run("Design Patterns");
     }
 }
