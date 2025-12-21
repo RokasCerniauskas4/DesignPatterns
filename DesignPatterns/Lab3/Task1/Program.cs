6aba3c234e3c279fff5f2cf8926cd347f8368c3b
Rokas Černiauskas <Rokascerniauskas4@gmail.com>
2025-12-21 14:40:11 +0200

    task1 part

---
+++ DesignPatterns/Lab3/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Task1;
+
+var shapes = new IShape[]
+{
+    new Sphere(3),
+    new Cube(4),
+    new Parallelepiped(2, 3, 4),
+    new Torus(5, 2)
+};
+
+var vv = new VolumeVisitor();
+
+foreach (var sh in shapes)
+{
+    Console.WriteLine($"{sh.GetType().Name}: {sh.Accept(vv)}");
+}
