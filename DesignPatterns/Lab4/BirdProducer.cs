b8b451fb182acf300f28b7ac732184549bad8420
Rokas Černiauskas <Rokascerniauskas4@gmail.com>
2026-01-24 10:12:25 +0200

    Solid

---
+++ DesignPatterns/Lab4/BirdProducer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Coding.SOLID
+{
+    public class BirdProducer
+    {
+        public IBird ProduceBird(string birdType)
+        {
+            if (birdType == "Pinguin")
+                return new Pinguin();
+
+            if (birdType == "Sparrow")
+                return new Sparrow();
+
+            throw new ArgumentException($"Unknown bird type: {birdType}", nameof(birdType));
+        }
+    }
+}
---
+++ DesignPatterns/Lab4/IBird.cs
@@ -0,0 +1,17 @@
+﻿namespace Coding.SOLID
+{
+    public interface IBird
+    {
+        void Sing();
+        void Dance();
+        void Walk();
+        void ProduceEgg();
+        void DefendEgg();
+        void SearchForSpause();
+    }
+
+    public interface IFlyingBird : IBird
+    {
+        void Fly();
+    }
+}
